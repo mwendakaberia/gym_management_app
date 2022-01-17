f9cba4ffee0d81c529812bf80429fbdad38990b7
Zach <zachary.6.smith@gmail.com>
2022-01-17 11:54:55 -0600

    firebase cli added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 
+///todo
 void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
